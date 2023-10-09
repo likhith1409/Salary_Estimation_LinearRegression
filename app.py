b8a91139a4adbf0a1c48760ef0a48cdf54ef8e2c
NANDIKONDA LIKHITH REDDY <91020626+likhith1409@users.noreply.github.com>
2023-10-09 11:59:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 
 import pickle
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
